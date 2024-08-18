e48dac1dd24e6850aa11c66205a06256437c4d52
Farmix2003 <ffarrux386@gmail.com>
2024-08-18 09:50:50 +0300

    Steps box animation

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,27 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", () => {
-    const foodBox = document.querySelector(".food-box"), sweetBox = document.querySelector(".sweet-box");
+    const foodBox = document.querySelector(".food-box"), sweetBox = document.querySelector(".sweet-box"), steps = document.querySelectorAll(".step1");
     const toggleBox = (box, otherBox) => {
         box.classList.add("toggled");
         otherBox.classList.remove("toggled");
     };
     foodBox.addEventListener("click", () => toggleBox(foodBox, sweetBox));
     sweetBox.addEventListener("click", () => toggleBox(sweetBox, foodBox));
+    if (steps.length === 0) {
+        console.error("No elements with the class 'step1' were found.");
+        return;
+    }
+    const animateSteps = (index) => {
+        if (index >= steps.length || !steps[index]) {
+            index = 0;
+        }
+        let currentStep = steps[index];
+        currentStep.classList.add("animate-scale");
+        setTimeout(() => {
+            currentStep.classList.remove("animate-scale");
+            animateSteps(index + 1);
+        }, 2000);
+    };
+    console.log(steps);
+    animateSteps(0);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const foodBox = document.querySelector(".food-box") as HTMLDivElement,
-    sweetBox = document.querySelector(".sweet-box") as HTMLDivElement;
+    sweetBox = document.querySelector(".sweet-box") as HTMLDivElement,
+    steps = document.querySelectorAll<HTMLDivElement>(".step1");
 
   const toggleBox = (box: HTMLDivElement, otherBox: HTMLDivElement) => {
     box.classList.add("toggled");
@@ -10,4 +11,27 @@ document.addEventListener("DOMContentLoaded", () => {
 
   foodBox.addEventListener("click", () => toggleBox(foodBox, sweetBox));
   sweetBox.addEventListener("click", () => toggleBox(sweetBox, foodBox));
+
+  if (steps.length === 0) {
+    console.error("No elements with the class 'step1' were found.");
+    return;
+  }
+
+  const animateSteps = (index: number) => {
+    if (index >= steps.length || !steps[index]) {
+      index = 0;
+    }
+
+    let currentStep = steps[index];
+
+    currentStep.classList.add("animate-scale");
+
+    setTimeout(() => {
+      currentStep.classList.remove("animate-scale");
+      animateSteps(index + 1);
+    }, 2000);
+  };
+  console.log(steps);
+
+  animateSteps(0);
 });
